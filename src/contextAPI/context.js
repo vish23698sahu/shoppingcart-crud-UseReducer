da256c9a8b48f0248ebc587398ddec968e15a318
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-02-16 22:58:52 +0530

    v - 5 global context

---
+++ src/contextAPI/context.js
@@ -0,0 +1,53 @@
+import React, { useState, useContext, useReducer, useEffect } from 'react'
+import cartItems from '../data/data'
+import reducer from '../reducer/reducer'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-useReducer-cart-project'
+const AppContext = React.createContext()
+
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0
+}
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clearCart = () => {
+    dispatch({ type: 'CLEAR_CART' })
+  }
+
+  const remove = (id) => {
+    dispatch({ type: 'REMOVE', payload: id })
+  }
+
+  const addToCart = (id) => {
+    dispatch({ type: 'ADD_TO_CART', payload: id })
+  }
+  const removeFromCart = (id) => {
+    dispatch({ type: 'REMOVE_FROM_CART', payload: id })
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        ...state,
+        clearCart,
+        remove,
+        addToCart,
+        removeFromCart
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
